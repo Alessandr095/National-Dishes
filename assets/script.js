e6b51b785564b844da9f9afd03ec04518e13a35d
Alessandr095 <>
2023-11-06 15:43:46 +0000

    labeled functions and variables in JS

--- assets/script.js
+++ assets/script.js
@@ -96,4 +96,44 @@ const questions = [
 const questionElement = document.getElementById("question"); //variable for the question title, for the a later function to replace the place holder text with the question text
 const answerButton = document.getElementById('answer-buttons'); // variable for the answer buttons, for the later function to replace the place holder text with the answer buttons text 
 const nextButton = document.getElementById("next-btn");// variable for the next button, for a later function of replacing the place holder next button to only appear after a answer button is clicked
+/**
+ * a function that when the quiz starts the score with be set to 0
+ */
+function startQuiz(){
 
+}
+/**
+ * a function that replaces the placeholder text for in the question variable
+ */
+function showQuestions(){
+
+}
+/**
+ * a function to remove the next button place holder 
+ * and removed it form view until an answer button is clicked
+ */
+function resetState(){
+
+}
+/**
+ * a function with variables to replace the place holder text for the answer buttons and 
+ * an if statement that checks if the answer clicked is correct or incorrect
+ */
+function selectAnswer(){
+
+}
+/**
+ * a function to show the score at the end of the game with a value out of 10
+ */
+function showScore(){
+
+}
+/**
+ * a function that make the next button functional to cycle through the 10 questions
+ */
+function handleNextButton(){
+
+}
+
+
+    
